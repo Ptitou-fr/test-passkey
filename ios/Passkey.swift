74893a3aca8e127728b4a40dc2522614e908269c
Patrick MENGARDUQUE <patrick@mengarduque.com>
2023-02-28 17:35:39 -0800

    rollback format

--- ios/Passkey.swift
+++ ios/Passkey.swift
@@ -106,7 +106,7 @@ class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, 
             // registration result
             didCompleteWithAuthorization(
                 [
-                    "credentialID": credentialRegistration.credentialID,
+                    "credentialID": credentialRegistration.credentialID.base64EncodedString(),
                     "response": [
                         "attestationObject": credentialRegistration.rawAttestationObject?.base64EncodedString() ?? "",
                         "clientData": credentialRegistration.rawClientDataJSON.base64EncodedString()
